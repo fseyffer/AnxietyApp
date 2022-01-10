b6238858bce84e3898299e4db102325b5987e3dd
Finn Seyffer <finnseyffer@gmail.com>
2022-01-10 02:30:26 -0500

    Initial Commit

---
+++ AnxietyApp/AnxietyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnxietyAppApp.swift
+//  AnxietyApp
+//
+//  Created by Finn Seyffer on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AnxietyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
